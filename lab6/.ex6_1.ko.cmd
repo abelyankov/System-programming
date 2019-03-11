f74dde43b8902445b54c3d7f4a63ba626bf75507
Arthur Belyankov <arthurbelyankove@gmail.com>
2019-03-12 04:30:51 +0600

    Lab6

---
+++ lab6/.ex6_1.ko.cmd
@@ -0,0 +1,1 @@
+cmd_/home/arthur/System-programming/System-programming/lab6/ex6_1.ko := ld -r -m elf_x86_64 -z max-page-size=0x200000 -T ./scripts/module-common.lds --build-id  -o /home/arthur/System-programming/System-programming/lab6/ex6_1.ko /home/arthur/System-programming/System-programming/lab6/ex6_1.o /home/arthur/System-programming/System-programming/lab6/ex6_1.mod.o ;  true
---
+++ lab6/.tmp_versions/ex6_1.mod
@@ -0,0 +1,3 @@
+/home/arthur/System-programming/System-programming/lab6/ex6_1.ko
+/home/arthur/System-programming/System-programming/lab6/ex6_1.o
+
