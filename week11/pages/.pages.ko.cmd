bc039f74e84eb991206e50ce1caee1b1e810bada
Arthur <arthurbelyankova@gmail.com>
2019-04-20 11:45:56 +0600

    week11

---
+++ week11/pages/.pages.ko.cmd
@@ -0,0 +1,1 @@
+cmd_/home/abelyankov/System-programming/week11/pages/pages.ko := ld -r -m elf_x86_64  -z max-page-size=0x200000 -T ./scripts/module-common.lds  --build-id  -o /home/abelyankov/System-programming/week11/pages/pages.ko /home/abelyankov/System-programming/week11/pages/pages.o /home/abelyankov/System-programming/week11/pages/pages.mod.o ;  true
---
+++ week11/pages/.tmp_versions/pages.mod
@@ -0,0 +1,3 @@
+/home/abelyankov/System-programming/week11/pages/pages.ko
+/home/abelyankov/System-programming/week11/pages/pages.o
+
